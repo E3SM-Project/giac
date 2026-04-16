e7e26af1a005fc0f8e6ae751fa191f1261276396
Sha Feng <bunnyvon@gmail.com>
2026-04-16 18:55:42 -0500

    modifications made for smoke tests to work

--- iac/coupling/mksurfdat.F90
+++ iac/coupling/mksurfdat.F90
@@ -407,7 +407,7 @@ subroutine mksurfdat_run(year,plodata)
     ! ----------------------------------------------------------------------
     
     ! Only read domain map once
-    if (domain_set(ldomain) == .false.) then 
+    if (.not.(domain_set(ldomain))) then
        write(6,*)'calling domain_read'
        if ( .not. domain_read_map(ldomain, fgrddat) )then
           call domain_read(ldomain, fgrddat)
