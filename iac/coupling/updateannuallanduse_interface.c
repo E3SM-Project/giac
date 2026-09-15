1892fae3ef0f62c3ba32d4c3fd5e797b22310726
Alan Di Vittorio <avdivittorio@lbl.gov>
2026-09-15 09:42:07 -0700

    Fixes gnu complie error related to fortran to c ehc interface 	modified:   iac/coupling/updateannuallanduse_interface.c

--- iac/coupling/updateannuallanduse_interface.c
+++ iac/coupling/updateannuallanduse_interface.c
@@ -2,13 +2,21 @@
 #define PLONFLDS 23
 
 /*  this declaration matches the one in updateannuallanduse_v2.c  */
+/*  since C is row major (last index varies fastest) the glmo/plodata pointers do not need to know the size
+ *  of the first (row) dimension here, as long at the code knows it for proper indexing. they need to know only
+ *  the size of the second dimension to know how long each row is */
 void updateannuallanduse_main(double glmo[][GLMONFLDS], double plodata[][PLONFLDS], int *inyear,
 			int *crop_addtreeonly, double *crop_setherbfracrem, double *crop_setavailtreefracrem,
                 int *pasture_addtreeonly, double *pasture_setherbfracrem, double *pasture_setavailtreefracrem);
 
-void updateannuallanduse_(double *glmo[][GLMONFLDS], double *plodata[][PLONFLDS], int *myear,
+/*  fortran passes a flat pointer to contiguous memory. it is column major (first index varies fastest) so
+ *  the fields vary fastest on disk because they are the first dim in the fortran code. the c code has fixed
+ *  2d arrays for glmo and plodata, with a pointer to the contiguous memory. this syntax should be more clear
+ *  and robust */
+void updateannuallanduse_(double *glmo, double *plodata, int *myear,
 		int *crop_addtreeonly, double *crop_setherbfracrem, double *crop_setavailtreefracrem,
 		int *pasture_addtreeonly, double *pasture_setherbfracrem, double *pasture_setavailtreefracrem) {
-    updateannuallanduse_main(glmo, plodata, myear, crop_addtreeonly, crop_setherbfracrem, crop_setavailtreefracrem,
-			pasture_addtreeonly, pasture_setherbfracrem, pasture_setavailtreefracrem);
+    updateannuallanduse_main((double (*)[GLMONFLDS]) glmo, (double (*)[PLONFLDS]) plodata, myear, crop_addtreeonly,
+		    crop_setherbfracrem, crop_setavailtreefracrem, pasture_addtreeonly, pasture_setherbfracrem,
+		    pasture_setavailtreefracrem);
 }
